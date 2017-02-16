ee51bdbda6f41b40226d05dd2da0375e0ec36726
Actine <actinate@gmail.com>
2017-02-16 11:03:03 +0200

    Почистив текст назад

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("dfghHello");
-        System.out.println("Hellosdf");
+        System.out.println("Hello");
     }
 
 }
